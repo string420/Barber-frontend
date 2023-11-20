2636a78ef33dc7a00623232a80086b877e7ae4ba
pfdelfin98 <pfdelfin98@gmail.com>
2023-11-20 14:06:45 +0800

    change dropdown to selectbox

--- src/components/face_detection/RenderFilter.tsx
+++ src/components/face_detection/RenderFilter.tsx
@@ -1,19 +1,18 @@
-import { useState, useRef } from "react";
+import { useState,useRef } from "react";
 
-interface Props {
+interface Prop {
   setBase64Image: (image: string) => void;
 }
 
-const RenderFilter = ({ setBase64Image }: Props) => {
+const RenderFilter = ({ setBase64Image }: Prop) => {
   const [showUploadButton, setShowUploadButton] = useState<boolean>(false);
-  const [showTakePhotoButton, setShowTakePhotoButton] =
-    useState<boolean>(false);
+  const [showTakePhotoButton, setShowTakePhotoButton] = useState<boolean>(false);
   const [capturedPhoto, setCapturedPhoto] = useState<string | null>(null);
   const [uploadedPhoto, setUploadedPhoto] = useState<string | null>(null);
-  const [showRadioButtons, setShowRadioButtons] = useState<boolean>(false);
-  const [showGenerate, setShowGenerate] = useState<boolean>(false);
-  const [showReset, setShowReset] = useState<boolean>(false);
-  const [showOr, setShowOr] = useState<boolean>(false);
+  const [showSelectBox, setShowSelectBox] = useState<boolean>(false); 
+  const [showGenerate, setShowGenerate] = useState<boolean>(false); 
+  const [showReset, setShowReset] = useState<boolean>(false); 
+  const [showOr, setShowOr] = useState<boolean>(false); 
   const [fileimg, setFile] = useState<File | null>(null);
 
   const handleRadioChange = (e: any) => {
@@ -22,15 +21,15 @@ const RenderFilter = ({ setBase64Image }: Props) => {
       setShowTakePhotoButton(true);
       setCapturedPhoto(null);
       setUploadedPhoto(null);
-      setShowRadioButtons(true);
-      setShowOr(true);
+      setShowSelectBox(true); 
+      setShowOr(true); 
     } else {
       setShowUploadButton(false);
       setShowTakePhotoButton(false);
       setCapturedPhoto(null);
       setUploadedPhoto(null);
-      setShowRadioButtons(false);
-      setShowOr(false);
+      setShowSelectBox(false); 
+      setShowOr(false); 
     }
   };
 
@@ -45,24 +44,23 @@ const RenderFilter = ({ setBase64Image }: Props) => {
       const canvas = document.createElement("canvas");
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
-      canvas
-        .getContext("2d")
-        ?.drawImage(video, 0, 0, canvas.width, canvas.height);
+      canvas.getContext("2d")?.drawImage(video, 0, 0, canvas.width, canvas.height);
       const photo = canvas.toDataURL("image/png");
       setCapturedPhoto(photo);
       setShowTakePhotoButton(true);
       setShowGenerate(true);
       setShowReset(true);
       setShowUploadButton(false);
-      setShowOr(false);
-      const blob = dataURLtoBlob(photo);
-      const file = new File([blob], "image.png", { type: "image/png" });
-      setFile(file);
+      setShowOr(false); 
+        const blob = dataURLtoBlob(photo);
+        const file = new File([blob], "image.png", { type: "image/png" });
+      setFile(file);  
       stream.getVideoTracks()[0].stop();
     } catch (error) {
       console.error("Error accessing camera:", error);
     }
   };
+  
 
   //convert Data URL to Blob
   function dataURLtoBlob(dataURL: string): Blob {
@@ -74,7 +72,8 @@ const RenderFilter = ({ setBase64Image }: Props) => {
     }
     return new Blob([ab], { type: "image/png" });
   }
-
+  
+  
   const handleUploadPhoto = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files && files.length > 0) {
@@ -93,48 +92,42 @@ const RenderFilter = ({ setBase64Image }: Props) => {
       reader.readAsDataURL(file);
     }
   };
-
+  
   const radioRef = useRef(null);
-  const [responseImage, setResponseImage] = useState("");
-  // const [showLoading, setShowLoading] = useState(true);
+  const selectRef = useRef(null);
+  const [responseImage, setResponseImage] = useState('');
   const [processing, setProcessing] = useState(false);
 
   const generateHaircut = async () => {
-    if (radioRef.current) {
-      const radioContainer = radioRef.current as HTMLElement;
-      const selectedOption = radioContainer.querySelector(
-        "input:checked"
-      ) as HTMLInputElement;
+    if (selectRef.current) {
+      const selectContainer = selectRef.current as HTMLElement;
+      const selectedOption = selectContainer.querySelector("select") as HTMLSelectElement;
       if (selectedOption) {
         const haircut = selectedOption.value.toString();
         const image = uploadedPhoto || capturedPhoto;
-
+  
         if (fileimg) {
           console.log("Haircut: ", haircut);
           console.log("Image: ", image);
-
+  
           const payload = new FormData();
           payload.append("haircut", haircut);
           payload.append("image", fileimg);
-
+  
           try {
             setProcessing(true);
-            const response = await fetch(
-              "https://flask-production-f2d6.up.railway.app/generate-haircut",
-              {
-                method: "POST",
-                body: payload,
-              }
-            );
-
+            const response = await fetch("https://flask-production-f2d6.up.railway.app/generate-haircut", {
+              method: "POST",
+              body: payload,
+            });
+  
             if (response.ok) {
               console.log("Request successful");
               const data = await response.json();
-
+  
               console.log("Response Data:", data);
               if (data.image_data) {
                 setResponseImage(data.image_data);
-                setBase64Image(data.image_data); // TODO: added by me
                 setProcessing(false);
               } else {
                 console.error("Image generation failed:", data.error_msg);
@@ -153,37 +146,30 @@ const RenderFilter = ({ setBase64Image }: Props) => {
           setProcessing(false);
         }
       } else {
-        console.error("No radio button selected.");
+        console.error("No option selected.");
         setProcessing(false);
       }
     } else {
-      console.error("Radio container not found.");
+      console.error("Select container not found.");
       setProcessing(false);
     }
   };
 
-  const retryAgain = () => {
-    setResponseImage("");
-    setUploadedPhoto("");
-    setCapturedPhoto(null);
-    setShowRadioButtons(true);
-    setShowTakePhotoButton(true);
-    setShowOr(true);
-    setShowUploadButton(true);
-    setShowGenerate(false);
-    setShowReset(false);
-  };
-
+const retryAgain = () => {
+  setResponseImage("");
+  setUploadedPhoto("");
+  setCapturedPhoto(null); 
+  setShowSelectBox(true);
+  setShowTakePhotoButton(true);
+  setShowOr(true);
+  setShowUploadButton(true);
+  setShowGenerate(false);
+  setShowReset(false); 
+};
+  
+  
   return (
-    <div
-      style={{
-        marginTop: "30px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
+    <div style={{ marginTop: "30px", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
       <div className="create-appoitment-yes-no-container">
         <span>
           Do you want to try our virtual filter that will apply your desired
@@ -210,166 +196,75 @@ const RenderFilter = ({ setBase64Image }: Props) => {
         </label>
       </div>
 
-      <div
-        style={{
-          marginTop: "30px",
-          borderRadius: "25px",
-          border: "5px solid #1a1919",
-          padding: "10px",
-        }}
-        className="radio-buttons-container"
-      >
-        {showRadioButtons && (
-          <div
-            style={{ marginTop: "10px" }}
-            className="radio-buttons-container"
-            ref={radioRef}
-          >
-            <label style={{ color: "white", fontSize: "20px" }}>
-              Select your Haircut:
-            </label>
-            <br></br>
-            <br></br>
-            <label>
-              <input type="radio" name="options" checked={true} value="1" />
-              <span style={{ color: "white" }}>Buzz Cut</span>
-            </label>
-            <label style={{ marginLeft: "15px" }}>
-              <input type="radio" name="options" value="2" />
-              <span style={{ color: "white" }}>Low Fade</span>
-            </label>
-            <label style={{ marginLeft: "15px" }}>
-              <input type="radio" name="options" value="3" />
-              <span style={{ color: "white" }}>Textured Fringe</span>
-            </label>
-            <label style={{ marginLeft: "15px" }}>
-              <input type="radio" name="options" value="4" />
-              <span style={{ color: "white" }}>Pompadour</span>
-            </label>
-            <label style={{ marginLeft: "15px" }}>
-              <input type="radio" name="options" value="5" />
-              <span style={{ color: "white" }}>Undercut</span>
-            </label>
-          </div>
-        )}
-        {showTakePhotoButton && (
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <button
-              className="create-appointment-btn"
-              onClick={handleTakePhoto}
-            >
-              Take a Photo
-            </button>
-          </div>
-        )}
+      <div style={{ marginTop: '30px', borderRadius:"25px",border: '5px solid #1a1919', padding: '10px' }} className="radio-buttons-container">
+
+      {showSelectBox && (
+  <div style={{ marginTop: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center' }} className="select-box-container" ref={selectRef}>
+    <label style={{ color: 'white', fontSize: '20px' }}>
+      Select your Haircut:
+    </label><br /><br />
+    <select name="haircutOptions" style={{ width: '150px', height:'35px', fontSize:'15px' }} >
+      <option value="1">Buzz Cut</option>
+      <option value="2">Low Fade</option>
+      <option value="3">Textured Fringe</option>
+      <option value="4">Pompadour</option>
+      <option value="5">Undercut</option>
+    </select>
+  </div>
+)}
+
+      {showTakePhotoButton && (
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+          <button className="create-appointment-btn" onClick={handleTakePhoto}>Take a Photo</button>
+        </div>
+      )}
         {showOr && (
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              marginTop: "10px",
-            }}
-          >
-            <label htmlFor="" style={{ textAlign: "center", color: "white" }}>
-              - or -
-            </label>
-          </div>
-        )}
-        ;
-        {capturedPhoto && (
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <img
-              style={{ width: "400px" }}
-              src={responseImage ? responseImage : capturedPhoto}
-              alt="Captured Photo"
-            />
-          </div>
-        )}
-        {showUploadButton && (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              marginTop: "-60px",
-            }}
-          >
-            <label
-              className="create-appointment-btn"
-              style={{ cursor: "pointer" }}
-            >
-              Upload Photo
-              <input
-                type="file"
-                style={{ display: "none" }}
-                accept="image/*"
-                onChange={handleUploadPhoto}
-              />
-            </label>
-            {uploadedPhoto && (
-              <img
-                src={responseImage ? responseImage : uploadedPhoto}
-                alt="Uploaded Photo"
-                style={{ marginTop: "10px", width: "400px" }}
-              />
-            )}
-          </div>
-        )}
-        ;
-        {showReset && (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              marginTop: "-60px",
-            }}
-          >
-            <button className="create-appointment-btn" onClick={retryAgain}>
-              Reset Image
-            </button>
-          </div>
-        )}
-        ;
-        {showGenerate && !responseImage && (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              marginTop: "-60px",
-            }}
-          >
-            {processing ? (
-              <button className="create-appointment-btn">Processing...</button>
-            ) : (
-              <button
-                className="create-appointment-btn"
-                onClick={generateHaircut}
-              >
-                Generate Haircut
-              </button>
-            )}
-          </div>
-        )}
-        ;
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", marginTop:"10px" }}>
+        <label htmlFor="" style={{textAlign:"center", color:"white"}}>- or -</label>
+        </div>
+      )};
+
+      {capturedPhoto && (
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <img
+      style={{ width:"400px" }}
+        src={responseImage ? responseImage : capturedPhoto}
+        alt="Captured Photo"
+      />      
+  </div>
+      )}
+
+    {showUploadButton && (
+  <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", marginTop:"-60px" }}>
+    <label className="create-appointment-btn" style={{ cursor: "pointer" }}>
+      Upload Photo
+      <input type="file" style={{ display: "none" }} accept="image/*" onChange={handleUploadPhoto} />
+    </label>
+    {uploadedPhoto && (
+      <img 
+      src={ responseImage ? responseImage : uploadedPhoto} 
+      alt="Uploaded Photo" style={{ marginTop: "10px", width:"400px" }} />
+
+    )}
+
+  </div>
+)};
+
+{showReset && (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", marginTop:"-60px" }}>
+        <button className="create-appointment-btn" onClick={retryAgain}>Reset Image</button>
+        </div>
+        )};
+ {showGenerate && !responseImage && (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", marginTop:"-60px" }}>
+        {processing ? (
+            <button className="create-appointment-btn">Processing...</button>
+          ) : (
+            <button className="create-appointment-btn" onClick={generateHaircut}>Generate Haircut</button>
+          )}        
       </div>
+        )};
+    </div>
     </div>
   );
 };
