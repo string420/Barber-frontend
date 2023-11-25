9a4ced89363a2f91f7187d52e9f7666567a79c57
Christ Joe Santos <christjoesantos2000@gmail.com>
2023-11-26 01:18:51 +0800

    revert to 216583872a40263e995984ddc20b6009e5734491

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import { useLocation, useNavigate } from "react-router-dom";
 import useAuthStore from "../../zustand/AuthStore";
-import { Person, Menu, Close } from "@mui/icons-material";
+import { Person, Menu } from "@mui/icons-material";
 import { Dialog, DialogContent } from "@mui/material";
 import Login from "../login/LoginModal";
 import { Transition, UserInterface } from "../../Types";
@@ -16,20 +16,18 @@ const Navbar = () => {
   const clearUser = useAuthStore((state) => state.clearUser);
   const [open, setOpen] = useState<boolean>(false);
   const [userData, setUserData] = useState<UserInterface>();
-
+  const [isNavMobile, setIsNavMobile] = useState<boolean>(false);
   const [showManagementDropdown, setShowManagementDropdown] =
     useState<boolean>(false);
 
   const location = useLocation();
 
-  //
-  const [click, setClick] = useState(false);
-
-  const handleClick = () => setClick(!click);
-  //
-
   const toggleManagementDropdown = () => {
     setShowManagementDropdown(!showManagementDropdown);
+  };
+
+  const toggleNavMobile = () => {
+    setIsNavMobile(!isNavMobile);
   };
 
   const toggleLoginModal = () => {
@@ -65,151 +63,149 @@ const Navbar = () => {
           />
         </div>
 
-        <div className={click ? "nav-menu active" : "nav-menu"}>
-          <div className="nav-link-container">
-            <a
-              className="navbar-links"
-              href={location.pathname === "/" ? "#" : "/"}
-              onClick={handleClick}
-            >
-              HOME
-            </a>
-
-            <a className="navbar-links" href="#story" onClick={handleClick}>
-              ABOUT
-            </a>
-            <a className="navbar-links" href="#gallery" onClick={handleClick}>
-              GALLERY
-            </a>
-            <a className="navbar-links" href="#service" onClick={handleClick}>
-              WHAT WE DO
-            </a>
-            <a className="navbar-links" href="/reviews" onClick={handleClick}>
-              REVIEWS
-            </a>
-            {userData?.role === "user" && (
+        <div
+          className={
+            isNavMobile
+              ? "navbar-mobile-link-container"
+              : "navbar-links-container"
+          }
+        >
+          <a
+            className="navbar-links"
+            href={location.pathname === "/" ? "#" : "/"}
+          >
+            HOME
+          </a>
+          <div className={click ? "nav-menu active" : "nav-menu"}>
+            <div className="nav-link-container">
               <a
                 className="navbar-links"
-                href="/appointments"
+                href={location.pathname === "/" ? "#" : "/"}
                 onClick={handleClick}
               >
-                APPOINTMENTS
+                HOME
               </a>
-            )}
 
-            <a className="navbar-links" href="/try-filter-camera">
-              TRY OUR FILTER
-            </a>
+              <a className="navbar-links" href="#story" onClick={handleClick}>
+                ABOUT
+              </a>
+              <a className="navbar-links" href="#gallery" onClick={handleClick}>
+                GALLERY
+              </a>
+              <a className="navbar-links" href="#service" onClick={handleClick}>
+                WHAT WE DO
+              </a>
+              <a className="navbar-links" href="/reviews" onClick={handleClick}>
+                REVIEWS
+              </a>
+              {userData?.role === "user" && (
+                <a
+                  className="navbar-links"
+                  href="/appointments"
+                  onClick={handleClick}
+                >
+                  APPOINTMENTS
+                </a>
+              )}
 
-            {userData?.role === "admin" && (
-              <div className="nav-dropdown" onClick={toggleManagementDropdown}>
-                <span className="navbar-links">MANAGEMENT</span>
-                {showManagementDropdown && (
-                  <div
-                    className="dropdown-content"
-                    style={
-                      showManagementDropdown
-                        ? { display: "block" }
-                        : { display: "none" }
-                    }
-                  >
-                    <Link
-                      style={{ textDecoration: "none" }}
-                      to="/admin/appointment"
-                      onClick={handleClick}
-                    >
-                      <span>APPOINTMENT MANAGEMENT</span>
-                    </Link>
-                    <Link
-                      style={{ textDecoration: "none" }}
-                      to="/admin/barbers"
-                      onClick={handleClick}
-                    >
-                      <span>BARBER MANAGEMENT</span>
-                    </Link>
-                    <Link
-                      style={{ textDecoration: "none" }}
-                      to="/admin/cuts"
-                      onClick={handleClick}
-                    >
-                      <span>CUT MANAGEMENT</span>
-                    </Link>
-                  </div>
-                )}
-              </div>
-            )}
-          </div>
+              <a className="navbar-links" href="/try-filter-camera">
+                TRY OUR FILTER
+              </a>
 
-          <div>
-            {user ? (
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "flex-start",
-                  flexDirection: "column",
-                  gap: "5px",
-                }}
-                className="navbar-user-log-container"
-              >
+              {userData?.role === "admin" && (
+                <div className="nav-dropdown" onClick={toggleManagementDropdown}>
+                  <span className="nav-link">MANAGEMENT</span>
+                  {showManagementDropdown && (
+                    <div
+                      className="dropdown-content"
+                      style={
+                        showManagementDropdown
+                          ? { display: "block" }
+                          : { display: "none" }
+                      }
+                    >
+                      <Link
+                        style={{ textDecoration: "none" }}
+                        to="/admin/appointment"
+                        onClick={handleClick}
+                      >
+                        <span>APPOINTMENT MANAGEMENT</span>
+                      </Link>
+                      <Link
+                        style={{ textDecoration: "none" }}
+                        to="/admin/barbers"
+                        onClick={handleClick}
+                      >
+                        <span>BARBER MANAGEMENT</span>
+                      </Link>
+                      <Link
+                        style={{ textDecoration: "none" }}
+                        to="/admin/cuts"
+                        onClick={handleClick}
+                      >
+                        <span>CUT MANAGEMENT</span>
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+
+            <div
+              className={
+                isNavMobile
+                  ? "navbar-mobile-login-container"
+                  : "navbar-login-container"
+              }
+            >
+              {user ? (
                 <div
                   style={{
                     display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    gap: "10px",
+                    alignItems: "flex-start",
+                    flexDirection: "column",
+                    gap: "5px",
                   }}
                 >
-                  <Person sx={{ color: "white" }} className="navbar-icon" />
-                  <span style={{ color: "white", fontSize: "18px" }}>
-                    {user}
-                  </span>
-                  <NotificationBadge />
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      gap: "10px",
+                    }}
+                  >
+                    <Person sx={{ color: "white" }} />
+                    <span style={{ color: "white", fontSize: "18px" }}>{user}</span>
+                    <NotificationBadge />
+                  </div>
+                  <button className="navbar-login-btn" onClick={clearUser}>
+                    LOGOUT
+                  </button>
                 </div>
-                <button className="navbar-login-btn" onClick={clearUser}>
-                  LOGOUT
+              ) : (
+                <button className="navbar-login-btn" onClick={() => setOpen(true)}>
+                  LOGIN
                 </button>
-              </div>
-            ) : (
-              <button
-                className="navbar-login-btn"
-                onClick={() => setOpen(true)}
-              >
-                LOGIN
-              </button>
-            )}
+              )}
+            </div>
+
+            <div className="navbar-burger-menu" onClick={toggleNavMobile}>
+              <Menu sx={{ color: "white", fontSize: "35px" }} />
+            </div>
           </div>
+          <Dialog
+            open={open}
+            onClose={toggleLoginModal}
+            TransitionComponent={Transition}
+            keepMounted
+          >
+            <DialogContent>
+              <Login toggleLoginModal={toggleLoginModal} />
+            </DialogContent>
+          </Dialog>
         </div>
-
-        {/* <div className="navbar-burger-menu" onClick={toggleNavMobile}>
-          <Menu sx={{ color: "white", fontSize: "35px" }} />
-        </div> */}
-
-        <div className="nav-icon" onClick={handleClick}>
-          {/* <i className={click ? "fas fa-times" : "fas fa-bars"}></i> */}
-
-          {click ? (
-            <span className="icon">
-              <Close sx={{ color: "white" }} />
-            </span>
-          ) : (
-            <span className="icon">
-              <Menu sx={{ color: "white" }} />
-            </span>
-          )}
-        </div>
-      </div>
-      <Dialog
-        open={open}
-        onClose={toggleLoginModal}
-        TransitionComponent={Transition}
-        keepMounted
-      >
-        <DialogContent>
-          <Login toggleLoginModal={toggleLoginModal} />
-        </DialogContent>
-      </Dialog>
-    </div>
-  );
+        );
 };
 
-export default Navbar;
+        export default Navbar;
